9c624159ac6d2201f5ffc3e167bddd3ced6f187e
Mazurov Anton <outwork.anton@gmail.com>
2018-12-14 00:13:31 +0300

    Initial refactorings

---
+++ app/src/main/java/outwork/metricflowandroidclient/MainActivity.kt
@@ -0,0 +1,21 @@
+package outwork.metricflowandroidclient
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.main.*
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
